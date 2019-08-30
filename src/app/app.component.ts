d3e8a72bbfca64e815b2c0d14fba5524d9424220
Alberto Siurob <alberto.siurob@kavak.com>
2019-08-30 17:56:55 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'siurob';
+
+  showNavigationArrows = true;
+  showNavigationIndicators = true;
+  images = [1, 2, 3, 4, 5].map(() => `https://picsum.photos/900/500?random&t=${Math.random()}`);
+  math: Math = Math;
+
+  ngOnInit() {
+    console.log( this.images );
+  }
 }
